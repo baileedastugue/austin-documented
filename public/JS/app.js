f131505c8b4658355a9a3a6756c059c74f1c11bb
Bailee Dastugue <bailee.dast@gmail.com>
2020-06-02 15:00:51 -0500

    toggle comments works better, default comment state, modal colors and hovers

--- public/JS/app.js
+++ public/JS/app.js
@@ -55,16 +55,19 @@ $(document).ready(() => {
                 <form>
                     <textarea id="bodyinput" name="body" placeholder="Add a comment" ></textarea>
                     
-                    <button type="submit" class="btn submit-btn btn-primary">Post</button>
+                    <button type="submit" class="btn submit-btn">Post</button>
                 </form>`
                 );
-            $("div.modal-footer").empty();
-            $("div.modal-footer").append(`
-                <span class="view-comments" state="hidden" data-id="${data[0]._id}">View Comments</span><br>
-                `);
-            $("div.modal-footer").append(`<br>
-                <div class="view-container" data-id="${data[0]._id}"></div>
-                `)
+            $("div.modal-body").append(`
+                <div class="row">
+                    <div class="col-12">
+                        <br>
+                        <div class="view-comments" state="hidden" data-id="${data[0]._id}">View Comments</div><br>
+                    
+                        <div class="view-container" data-id="${data[0]._id}"></div>
+                    </div>
+                </div>`);
+                
             $("div.modal-body").append(``);
             $(".submit-btn").attr("data-id", data[0]._id);
         })
@@ -87,7 +90,7 @@ $(document).ready(() => {
         })
     })
 
-    $(document).on("click", "span.view-comments", function() {
+    $(document).on("click", "div.view-comments", function() {
         var articleId = $(this).attr("data-id");
         var thisContainer = ".view-container[data-id=" + articleId +"]"; 
         // console.log(articleId);
@@ -133,42 +136,51 @@ $(document).ready(() => {
         }
     })
 
-//     $(document).on("click", ".toggle-comments", function() {
-//         var articleId = $(this).attr("data-id");
-//         var thisContainer = ".comments-container[data-id=" + articleId +"]"; 
-//         console.log(articleId);
-//         if ($(this).attr("state") === "hidden") {
-//             $(this).attr("state", "shown");
-//             $(thisContainer).show();
-//             $(thisContainer).empty();
-//             // $(thisContainer).css("display", "block");
-//             $(this).text("Hide Comments");
-//             $.ajax({
-//                 method: "GET",
-//                 url: "/articles/" + articleId
-//             }).then(data => {
-//                 console.log(thisContainer);
-//                 for (let i = 0; i < data[0].comment.length; i++) {
-//                     let commentArray = data[0].comment;
-                    
-//                     $(thisContainer).append(`<div class="card"> 
-//                         <div class="card-body">
-//                             <div class="card-text">
-//                                 ${commentArray[i].body}
-//                             </div>
-//                         </div>    
-//                     </div>`)
-//                     console.log(commentArray[i].body);
-//                 }
-//             })
-//         }
-//         else {
-//             $(this).attr("state", "hidden");
-//             console.log("hiding comments");
-//             $(this).text("View Comments");
-//             $(thisContainer).hide();
-//         }
-//     })
+    $(document).on("click", ".toggle-comments", function() {
+        var articleId = $(this).attr("data-id");
+        var thisContainer = ".comments-container[data-id=" + articleId +"]"; 
+        console.log(articleId);
+        if ($(this).attr("state") === "hidden") {
+            $(this).attr("state", "shown");
+            $(thisContainer).show();
+            $(thisContainer).empty();
+            // $(thisContainer).css("display", "block");
+            $(this).text("Hide Comments");
+            $.ajax({
+                method: "GET",
+                url: "/articles/" + articleId
+            }).then(data => {
+                if (data[0].comment.length === 0) {
+                    $(thisContainer).append(`<div class="card"> 
+                        <div class="card-body">
+                            <div class="card-text">
+                                No comments posted yet!
+                            </div>
+                        </div>    
+                    </div>`)
+                } else {
+                    for (let i = 0; i < data[0].comment.length; i++) {
+                        let commentArray = data[0].comment;
+                        
+                        $(thisContainer).append(`<div class="card"> 
+                            <div class="card-body">
+                                <div class="card-text">
+                                    ${commentArray[i].body}
+                                </div>
+                            </div>    
+                        </div>`)
+                        console.log(commentArray[i].body);
+                    }
+                }
+            })
+        }
+        else {
+            $(this).attr("state", "hidden");
+            console.log("hiding comments");
+            $(this).text("View Comments");
+            $(thisContainer).hide();
+        }
+    })
 
 
 })
